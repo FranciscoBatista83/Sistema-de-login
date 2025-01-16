89be8d6ccc2d43fe0c49a7c60961d8a450efd87e
Franciscobatista83 <chicaobatista1983@gmail.com>
2025-01-16 09:16:48 -0300

    atualizaçoes

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document
+  .getElementById('formularioLogin')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+    const username = document.getElementById('usuario').value;
+    const password = document.getElementById('senha').value;
+    const errorMessage = document.getElementById('mensagemErro');
+
+    if (username === 'admin' && password === '1234') {
+      window.location.href = 'formulario_cadastro.html';
+      errorMessage.textContent = '';
+    } else {
+      errorMessage.textContent = 'Usuário ou senha incorretos.';
+    }
+  });
